753b5afa9e8d5b7702df2f15818d27b003871693
Tyler Romero <tyler.alexander.romero@gmail.com>
2017-09-07 10:22:23 -0700

    Small changes

--- sql/tensorflow_with_retraining_execute.sql
+++ sql/tensorflow_with_retraining_execute.sql
@@ -1,7 +1,7 @@
 USE [land_use_database]
 GO
 
---exec [dbo].[PreprocessingTF];
---exec [dbo].[RetrainTF];
+exec [dbo].[PreprocessingTF];
+exec [dbo].[RetrainTF];
 exec [dbo].[ScoreTF];
 exec [dbo].[EvaluateTF];
